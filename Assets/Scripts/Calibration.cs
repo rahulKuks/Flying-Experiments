3eb9f108d799c0136ea1641ff02a13d9ae71f578
William Yao <williamyao1994@gmail.com>
2017-05-31 14:58:30 -0700

    calibrating for a sedentary position

---
+++ Assets/Scripts/Calibration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calibration : MonoBehaviour
+{
+
+    [SerializeField] private Camera head;
+    [SerializeField] SteamVR_TrackedObject left_trackedObj;
+    [SerializeField] SteamVR_TrackedObject right_trackedObj;
+
+    private SteamVR_Controller.Device leftConroller_Device, rightController_Device;
+
+    public Transform normal, reclined;
+
+  // Use this for initialization
+    void Start()
+    {
+        leftConroller_Device = SteamVR_Controller.Input((int)left_trackedObj.index);
+        rightController_Device = SteamVR_Controller.Input((int)right_trackedObj.index);
+    }
+
+    void Update() 
+    {
+        if (leftConroller_Device == null || rightController_Device == null)
+        {
+            Debug.LogError("Null controllers");
+            return;
+        }
+
+        if (leftConroller_Device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            normal = Camera.main.transform;
+        }
+        if (rightController_Device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            reclined = Camera.main.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardController : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	void Update () {
+		Vector3 position = this.transform.position;
+		if (Input.GetKey(KeyCode.W)) 
+		{
+				position.z += speed;
+				this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.S)) 
+		{
+				position.z -= speed;
+				this.transform.position = position;
+		} 
+		if (Input.GetKey(KeyCode.A)) 
+		{
+				position.x -= speed;
+				this.transform.position = position;
+		} 
+		if (Input.GetKey(KeyCode.D)) 
+		{
+				position.x += speed;
+				this.transform.position = position;
+		} 
+		if (Input.GetKey(KeyCode.Q)) 
+		{
+				position.y -= speed;
+				this.transform.position = position;
+		} 
+		if (Input.GetKey(KeyCode.E)) 
+		{
+				position.y += speed;
+				this.transform.position = position;
+		}
+	}
+}
